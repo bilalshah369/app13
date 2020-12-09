fcea432654d72b9c3a9aa949a3b6ae279384a9eb
bilal shah <bilalshah369@yahoo.com>
2020-12-09 11:19:51 +0530

    code upload

---
+++ Models/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace App13.Models
+{
+    public partial class Registration
+    {
+        public int ProfileId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Gender { get; set; }
+        public string Adress { get; set; }
+    }
+}
